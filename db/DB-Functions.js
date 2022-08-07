f2fdd7c667c8c664fe8bb385018507b83893403c
Lukhanyo Vakele <lukhanyovakele@gmail.com>
2022-08-07 18:47:24 +0200

    now using pg-promise in my queries

--- db/DB-Functions.js
+++ db/DB-Functions.js
@@ -1,17 +1,16 @@
 const DBFunctions = (db) => {
     const add = async (reg) => {
-        let town_id = await db.query("SELECT id FROM towns WHERE town_code = $1", [reg.slice(0,2)])
-        town_id = town_id[0].id
-        await db.query("INSERT INTO reg_numbers (reg_number, town_id) VALUES($1, $2)", [reg, town_id])
+        let town_id = await db.oneOrNone("SELECT id FROM towns WHERE town_code = $1", [reg.slice(0,2)])
+        if(town_id === null) return
+        await db.none("INSERT INTO reg_numbers (reg_number, town_id) VALUES($1, $2)", [reg, town_id.id])
     }
     const getAll = async () => {
-        const results = await db.query("SELECT * FROM reg_numbers;")
+        const results = await db.any("SELECT * FROM reg_numbers;")
         return results
     }
     const filterByCode = async (code) => {
-        let town_id = await db.query("SELECT id FROM towns WHERE town_code = $1", [code])
-        town_id = town_id[0].id
-        let results = await db.query("SELECT * FROM reg_numbers WHERE town_id = $1", [town_id])
+        let town_id = await db.one("SELECT id FROM towns WHERE town_code = $1", [code])
+        let results = await db.any("SELECT * FROM reg_numbers WHERE town_id = $1", [town_id.id])
         return results
     }
     return {
