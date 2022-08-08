9dd22766596a2a9e3698489c74eac7fe34389245
Lukhanyo Vakele <lukhanyovakele@gmail.com>
2022-08-08 12:11:10 +0200

    use specific columns for my queries

--- db/DB-Functions.js
+++ db/DB-Functions.js
@@ -5,7 +5,7 @@ const DBFunctions = (db) => {
         await db.none("INSERT INTO reg_numbers (reg_number, town_id) VALUES($1, $2)", [reg, town_id.id])
     }
     const getAll = async () => {
-        const results = await db.any("SELECT * FROM reg_numbers;")
+        const results = await db.any("SELECT reg_number, town_id FROM reg_numbers;")
         return results
     }
     const getAllTowns = async () => {
@@ -14,7 +14,7 @@ const DBFunctions = (db) => {
     }
     const filterByCode = async (code) => {
         let town_id = await db.one("SELECT id FROM towns WHERE town_code = $1", [code])
-        let results = await db.any("SELECT * FROM reg_numbers WHERE town_id = $1", [town_id.id])
+        let results = await db.any("SELECT reg_number, town_id FROM reg_numbers WHERE town_id = $1", [town_id.id])
         return results
     }
     return {
